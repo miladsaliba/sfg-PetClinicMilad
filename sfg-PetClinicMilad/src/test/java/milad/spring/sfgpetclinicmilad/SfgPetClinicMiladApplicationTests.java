99e3429f018cc564554d013519ea499e93d3da8e
Milad <miladsaliba@hotmail.com>
2023-07-05 16:37:05 -0400

    initial project add

---
+++ sfg-PetClinicMilad/src/test/java/milad/spring/sfgpetclinicmilad/SfgPetClinicMiladApplicationTests.java
@@ -0,0 +1,13 @@
+package milad.spring.sfgpetclinicmilad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgPetClinicMiladApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
